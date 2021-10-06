41bceb401dc397fdcca216974c5d625a1834fbba
AYogore <58088654+AYogore@users.noreply.github.com>
2021-10-06 15:02:07 -0500

    generated nav and login component
    make sure to open in frontend folder

--- src/main/java/static/frontend/src/app/app.module.ts
+++ src/main/java/static/frontend/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav/nav.component';
+import { LoginComponent } from './login/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
