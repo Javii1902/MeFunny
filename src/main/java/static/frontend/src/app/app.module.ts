adb646b5c3c31902079790771409a057e1bf7df7
AYogore <58088654+AYogore@users.noreply.github.com>
2021-10-07 18:15:34 -0500

    rearranged

--- src/main/java/static/frontend/src/app/app.module.ts
+++ src/main/java/static/frontend/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav/nav.component';
-import { LoginComponent } from './login/login/login.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
