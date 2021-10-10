ca07544173f6e9f35fe3437e921cd631e84c188b
AYogore <58088654+AYogore@users.noreply.github.com>
2021-10-10 17:28:43 -0500

    frontend login and angular running

--- src/main/java/static/frontend/src/app/app.module.ts
+++ src/main/java/static/frontend/src/app/app.module.ts
@@ -1,18 +1,27 @@
+;
+import { CustomPipePipe } from './custom-pipe.pipe';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav/nav.component';
 import { LoginComponent } from './login/login.component';
+import { MemeComponent } from './meme/meme.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    LoginComponent
+    LoginComponent,
+    MemeComponent,
+    HomeComponent,
+    CustomPipePipe,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
