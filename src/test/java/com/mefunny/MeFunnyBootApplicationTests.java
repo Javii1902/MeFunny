591d7c20b6594f51fbde7d30ce0429df64efbfdc
Juan Garcia <57228962+Javii1902@users.noreply.github.com>
2021-10-11 20:59:34 -0500

    Added test for the User and Meme Repos.

--- src/test/java/com/mefunny/MeFunnyBootApplicationTests.java
+++ src/test/java/com/mefunny/MeFunnyBootApplicationTests.java
@@ -1,12 +1,10 @@
 package com.mefunny;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 import java.util.Arrays;
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.Mockito.when;
-
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -16,9 +14,13 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.mefunny.controller.MemeController;
 import com.mefunny.controller.UserController;
+import com.mefunny.model.Meme;
 import com.mefunny.model.User;
+import com.mefunny.repository.MemeRepository;
 import com.mefunny.repository.UserRepository;
+import com.mefunny.service.MemeService;
 import com.mefunny.service.UserService;
 
 @SpringBootTest
@@ -27,11 +29,20 @@ class MeFunnyBootApplicationTests {
 	@InjectMocks
 	private static UserService userService;
 	
+	@InjectMocks
+	private static MemeService memeService;
+	
+	@Mock
+	private static MemeRepository memeRepository;
+	
 	@Mock
 	private static UserRepository userRepository;
 	
 	@Autowired
 	private UserController userController;
+	
+	@Autowired
+	private MemeController memeController;
 	
 	@BeforeAll
 	public static void setUp() {
@@ -41,7 +52,10 @@ class MeFunnyBootApplicationTests {
 	@Test
 	public void contenxtLoads() throws Exception{
 		assertThat(userController).isNotNull();
+		assertThat(memeController).isNotNull();
 	}
+	
+	
 	
 	@Test
 	public void testFindAllUsers() {
@@ -60,15 +74,51 @@ class MeFunnyBootApplicationTests {
 	
 	@Test
 	public void testUserSave() {
-		User user = new User(1,"","");
-		
-		//Mockito.when(userService.save(Mockito.any(User.class))).thenReturn(user);
+		//Mockito.when(userService.save(Mockito.any(User.class))).thenReturn(new User());
 	}
 	
 	@Test
 	public void testFindByUserId() {
-		User user = new User();
+		//User user = new User();
 		
-		Mockito.when(userService.findByUserId(Mockito.any(Integer.class))).thenReturn(user);
+		Mockito.when(userService.findByUserId(Mockito.any(Integer.class))).thenReturn(new User());
+		User user = userService.findByUserId(Mockito.any(Integer.class));
+		assertThat(user).isNotNull();
 	}
+	
+	public void testFindByUserName() {
+		Mockito.when(userService.findByUserName(Mockito.any(String.class))).thenReturn(new User());
+		User user = userService.findByUserName(Mockito.any(String.class));
+		assertThat(user).isNotNull();
+	}
+	
+	@Test
+	public void testFindAllMemes() {
+		//Meme memes = new Meme();
+		Mockito.when(memeService.findAll()).thenReturn(
+				Arrays.asList(
+						new Meme(1,"","",1,1,""),
+						new Meme(2,"","",2,2,""),
+						new Meme(3,"","",3,3,""),
+						new Meme(4,"","",4,4,"")
+						)
+				);
+		List<Meme> memes = memeService.findAll();
+		assertThat(memes).isNotNull();
+	}
+	public void testMemeSave() {
+		//Mockito.when(memeService.save(Mockito.any(Meme.class))).thenAnswer(new Meme());
+	}
+	
+	public void testFindMemeByID() {
+		Mockito.when(memeService.findById(Mockito.any(Integer.class))).thenReturn(new Meme());
+		Meme meme = memeService.findById(Mockito.any(Integer.class));
+		assertThat(meme).isNotNull();
+	}
+	public void testFindMemeByName() {
+		Mockito.when(memeService.findByName(Mockito.any(String.class))).thenReturn(new Meme());
+		Meme meme = memeService.findByName(Mockito.any(String.class));
+		assertThat(meme).isNotNull();
+	}
+	
 }
