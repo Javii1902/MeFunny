b1536dac9c1ba2de2f8454b0d1557eebdf7d6cde
Juan Garcia <57228962+Javii1902@users.noreply.github.com>
2021-10-16 18:51:24 -0500

    these are the clean files

---
+++ src/main/java/com/mefunny/MeFunnyApplication.java
@@ -0,0 +1,13 @@
+package com.mefunny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeFunnyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeFunnyApplication.class, args);
+	}
+
+}
