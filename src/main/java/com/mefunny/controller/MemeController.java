ad8ec8c14d6731bb3633eaf8c53b1c5faad0e030
AYogore <58088654+AYogore@users.noreply.github.com>
2021-10-17 13:44:50 -0500

    git ignore data, and added crossorigin mapping to backend controller

--- src/main/java/com/mefunny/controller/MemeController.java
+++ src/main/java/com/mefunny/controller/MemeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.mefunny.model.Meme;
 import com.mefunny.service.MemeService;
 
-
+@CrossOrigin(origins = "*")
 @RestController("memeController")
 @RequestMapping("/meme")
 public class MemeController {
--- src/main/java/com/mefunny/controller/UserController.java
+++ src/main/java/com/mefunny/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.mefunny.model.User;
 import com.mefunny.service.UserService;
 
-
+@CrossOrigin(origins = "*")
 @RestController("userController")
 @RequestMapping("/user")
 public class UserController {
